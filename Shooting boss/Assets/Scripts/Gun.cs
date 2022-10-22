12b084b405f38c0d91ce454b551b6cb958539f60
DaoQuocViet18 <108569142+DaoQuocViet18@users.noreply.github.com>
2022-10-22 17:37:52 +0700

    Shoot_Animaton_ShootLine

--- Shooting boss/Assets/Scripts/Gun.cs
+++ Shooting boss/Assets/Scripts/Gun.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [Header("Shooting")]
     public GameObject bullet;
+    public Transform Cam;
     public Transform posShoot;
+    public bool readyToShoot;
     public float shootForce;
     public KeyCode shootKey = KeyCode.Mouse0;
 
+    public Animator anim;
+
     void Start()
     {
-        
+        anim = GameObject.Find("Weapon").GetComponent<Animator>();
+        readyToShoot = true;
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(shootKey))
+            RaycastHit hit;
+
+        if (Input.GetKeyDown(shootKey) && readyToShoot == true && Physics.Raycast(Cam.position, Cam.forward, out hit))
+        {
+            Vector3 targetLine = (hit.point - posShoot.position).normalized;
+            GameObject currentBul = Instantiate(bullet, posShoot.position, bullet.transform.rotation);
+            Rigidbody projectRb = currentBul.GetComponent<Rigidbody>();
+
+            projectRb.AddForce(targetLine * shootForce, ForceMode.Impulse);
+            anim.SetBool("Shooting", true);
+            readyToShoot = false;
+            StartCoroutine(End_Shoot());
+        }
+        else if (Input.GetKeyDown(shootKey) && readyToShoot == true)
         {
             GameObject currentBul = Instantiate(bullet, posShoot.position, bullet.transform.rotation);
             Rigidbody projectRb = currentBul.GetComponent<Rigidbody>();
 
-            projectRb.AddForce(posShoot.forward * shootForce, ForceMode.Impulse);
+            projectRb.AddForce(Cam.forward * shootForce, ForceMode.Impulse);
+            anim.SetBool("Shooting", true);
+            readyToShoot = false;
+            StartCoroutine(End_Shoot());
+        }
 
+        IEnumerator End_Shoot ()
+        {
+            yield return new WaitForSeconds(0.1f);
+            anim.SetBool("Shooting", false);
+            readyToShoot = true;
         }
     }
 }
