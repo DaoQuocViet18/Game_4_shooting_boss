9c48d42bfe5f7c6125bdbd96680a975c138db4f9
DaoQuocViet18 <108569142+DaoQuocViet18@users.noreply.github.com>
2022-11-12 20:58:38 +0700

    end_day

--- Shooting boss/Assets/Scripts/Weapon/Shooting.cs
+++ Shooting boss/Assets/Scripts/Weapon/Shooting.cs
@@ -46,68 +46,73 @@ public class Shooting : MonoBehaviour
             {
                 for (int i = 0; i < weapon.Length; i++)
                     if (weaponSwitching.selectWeapon == i && IsReady[i] == true)
-                    {
                         Shoot(hit.point, shootForce[i]);
-                        Action();
-                    }
             }
             else if (Input.GetKeyDown(shootKey))
             {
                 Vector3 target = transform.position + transform.forward * 20;
                 for (int i = 0; i < weapon.Length; i++)
                     if (weaponSwitching.selectWeapon == i && IsReady[i] == true)
-                    {
                         Shoot(target, shootForce[i]);
-                        Action();
-                    }
             }
         }
-
-    }
-        void Shoot (Vector3 target, float shootForce)
-        {
-            Vector3 targetLine = (target - posShoot.position).normalized;
-            GameObject currentBul = Instantiate(bullet[weaponSwitching.selectWeapon], posShoot.transform.position, Cam.transform.rotation);
-            Rigidbody projectRb = currentBul.GetComponent<Rigidbody>();
-
-            projectRb.AddForce(targetLine * shootForce, ForceMode.Impulse);
-        }
-
-        void Action ()
-        {
-            if (weaponSwitching.selectWeapon == 0 && IsReady[0] == true)
-                Gun();
-            else if (weaponSwitching.selectWeapon == 1 && IsReady[1] == true)
-                Boomerang();
         else if (weaponSwitching.selectWeapon == 2 && IsReady[2] == true)
-            Boomerang();
+        {
+            if (Input.GetKeyDown(shootKey))
+                StartCoroutine(Sword());
+        }   
+
+
+
+    }
+    void Shoot(Vector3 target, float shootForce)
+    {
+        Vector3 targetLine = (target - posShoot.position).normalized;
+        GameObject currentBul = Instantiate(bullet[weaponSwitching.selectWeapon], posShoot.transform.position, Cam.transform.rotation);
+        Rigidbody projectRb = currentBul.GetComponent<Rigidbody>();
+
+        projectRb.AddForce(targetLine * shootForce, ForceMode.Impulse);
+        Action();
     }
 
-        void Gun ()
-        {
-            IsReady[0] = false;
-            anim.SetBool("Shooting", true);
-            Invoke("End_Shoot", 0.1f);
-        }
+    void Action()
+    {
+        if (weaponSwitching.selectWeapon == 0 && IsReady[0] == true)
+            StartCoroutine(Gun());
+        else if (weaponSwitching.selectWeapon == 1 && IsReady[1] == true)
+            Boomerang();
+        else if (weaponSwitching.selectWeapon == 2 && IsReady[2] == true)
+            StartCoroutine(Sword());
+    }
 
-        void Boomerang ()
-        {
-            weapon[1].SetActive(false);
-            IsReady[1] = false;
-        }
+    IEnumerator Gun()
+    {
+        IsReady[0] = false;
+        anim.SetBool("Shooting", true);
+        yield return new WaitForSeconds(0.1f);
+        anim.SetBool("Shooting", false);
+        IsReady[0] = true;
+    }
 
-        public  void ResetBoomerang ()
-        {
-            weapon[0].SetActive(false);
-            weaponSwitching.selectWeapon = 1;
+    void Boomerang()
+    {
+        weapon[1].SetActive(false);
+        IsReady[1] = false;
+    }
+    public void ResetBoomerang()
+    {
+        weapon[0].SetActive(false);
+        weaponSwitching.selectWeapon = 1;
 
-            weapon[1].SetActive(true);
-            IsReady[1] = true;
-        }
+        weapon[1].SetActive(true);
+        IsReady[1] = true;
+    }
 
-        void  End_Shoot ()
-        {
-            anim.SetBool("Shooting", false);
-            IsReady[0] = true;
-        }
+
+    IEnumerator Sword()
+    {
+        anim.SetInteger("Sword", 1);
+        yield return new WaitForSeconds(1f);
+        anim.SetInteger("Sword", 0);
+    }    
 }
